4a977a43625c9a7b2fda39c5a23ba6556a58e215
Simon Gredal <simon.gredal@icloud.com>
2021-10-29 11:51:16 +0200

    Init Spring Project

---
+++ src/main/java/gruppe8/project_wishlist/ProjectWishlistApplication.java
@@ -0,0 +1,13 @@
+package gruppe8.project_wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWishlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectWishlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gruppe8/project_wishlist/ProjectWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe8.project_wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWishlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
