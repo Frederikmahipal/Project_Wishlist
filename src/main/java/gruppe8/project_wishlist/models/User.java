53cae50ac68ffc707b6570518801304e5397087b
Simon Gredal <simon.gredal@icloud.com>
2021-11-02 16:05:07 +0100

    User now implements UserDetails interface

--- src/main/java/gruppe8/project_wishlist/models/User.java
+++ src/main/java/gruppe8/project_wishlist/models/User.java
@@ -1,10 +1,20 @@
 package gruppe8.project_wishlist.models;
 
-public class User {
-    private int id;
-    private String fullName, email, argon2Password;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-    public User(Integer id, String name, String email, String password) {
+import java.util.Collection;
+import java.util.List;
+
+public class User implements UserDetails {
+    private Long id;
+    private String fullName;
+    private String email;
+    private String argon2Password;
+
+    public User(Long id, String name, String email, String password) {
         this.id = id;
         this.fullName = name;
         this.email = email;
@@ -17,11 +27,46 @@ public class User {
         this.argon2Password = password;
     }
 
-    public int getId() {
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.argon2Password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of( new SimpleGrantedAuthority("ROLE_USER") );
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -47,5 +92,15 @@ public class User {
 
     public void setArgon2Password(String argon2Password) {
         this.argon2Password = argon2Password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", argon2Password='" + argon2Password + '\'' +
+                '}';
     }
 }
