2b771e8cdefc582059b29047f6dbfc574c24a884
Simon Gredal <simon.gredal@icloud.com>
2021-11-02 16:08:05 +0100

    UserRepository changes

--- src/main/java/gruppe8/project_wishlist/repositories/UserRepository.java
+++ src/main/java/gruppe8/project_wishlist/repositories/UserRepository.java
@@ -7,19 +7,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserRepository {
 
-    @Autowired
-    private UserRepository() {
-    }
-
-    public User getUserByEmail(String email) {
-        if (email.equals("simon.gredal@icloud.com")) {
-            return new User(
-                    1,
-                    "Simon Gredal",
-                    email,
-                    "$argon2id$v=19$m=65546,t=10,p=1$4+LJ3HF7sLBx3rN0fAWYfA$80uIctb6p5b14H61LuU++l/Ds1mJ7PvWxJ8BWp47jCQ"
-            );
-        }
-        return null;
-    }
+    public static final User simon = new User(
+            1L,
+            "Simon Gredal",
+            "simon.gredal@icloud.com",
+            "$argon2id$v=19$m=65546,t=10,p=1$4+LJ3HF7sLBx3rN0fAWYfA$80uIctb6p5b14H61LuU++l/Ds1mJ7PvWxJ8BWp47jCQ"
+    );
 }
