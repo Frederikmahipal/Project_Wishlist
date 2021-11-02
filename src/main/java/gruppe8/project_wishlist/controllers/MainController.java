dac72b3e72608b0af85604f6db66b3aaa056639f
Simon Gredal <simon.gredal@icloud.com>
2021-11-02 16:12:49 +0100

    Add signed-in user to model when accesssing homepage

--- src/main/java/gruppe8/project_wishlist/controllers/MainController.java
+++ src/main/java/gruppe8/project_wishlist/controllers/MainController.java
@@ -1,33 +1,32 @@
 package gruppe8.project_wishlist.controllers;
 
-import com.sun.tools.javac.Main;
 import gruppe8.project_wishlist.models.User;
-import gruppe8.project_wishlist.services.UserAuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
 
 @Controller
 public class MainController {
-    private final UserAuthenticationService authService;
 
     @Autowired
-    private MainController(UserAuthenticationService authService) {
-        this.authService = authService;
+    private MainController() {
     }
 
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute(
+                "user",
+                SecurityContextHolder.getContext().getAuthentication().getPrincipal()
+        );
+        return "home";
     }
 
     @GetMapping("/registration")
@@ -38,17 +37,6 @@ public class MainController {
     @GetMapping("/login")
     public String login(){
         return "login";
-    }
-
-    @PostMapping("login")
-    public String login(@RequestParam String email, @RequestParam String password, HttpServletResponse response) {
-        User user = authService.authenticateUser(email, password);
-        if (user == null) {
-            System.out.println("login error");
-            return "index";
-        }
-        System.out.println("login success");
-        return "index";
     }
 
 
